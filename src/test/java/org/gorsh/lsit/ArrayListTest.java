8f7216a7b05e6f893781e0edc6c10c1e5ffc870e
asgor <alexmilesjg@gmail.com>
2023-03-09 16:56:37 +0500

    doc

--- src/test/java/org/gorsh/lsit/ArrayListTest.java
+++ src/test/java/org/gorsh/lsit/ArrayListTest.java
@@ -6,20 +6,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayListTest {
 
+    ArrayList<Integer> testNumbersList = new ArrayList<>();
+    ArrayList<String> testStringList = new ArrayList<>();
+
+
     @Test
-    void addAndGet() {
-        ArrayList <String> example = new ArrayList<>();
-        String str = "One";
-        String str1 = "Two";
-        String str2 = "Three";
+    void add() {
+        for (int i = 0; i < 10; i++) {
+            testNumbersList.add(i);
+        }
 
-        example.add(str);
-        example.add(str1);
-        example.add(str2);
+        testNumbersList.add(11);
 
-        assertTrue(str.equals(example.get(0)));
-        assertTrue(str1.equals(example.get(1)));
-        assertTrue(str2.equals(example.get(2)));
+        assertEquals(11, testNumbersList.size());
+    }
 
+    @Test
+    void get() {
+        testNumbersList.add(42);
+
+        assertEquals(42, testNumbersList.get(0));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> testNumbersList.get(10));
+    }
+
+    @Test
+    void remove() {
+        testNumbersList.add(42);
+        testNumbersList.add(0);
+
+        testNumbersList.remove(0);
+
+        assertEquals(1, testNumbersList.size());
+        assertEquals(0, testNumbersList.get(0));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> testNumbersList.remove(10));
+    }
+
+    @Test
+    void testNumbersSort() {
+        for (int i = 10; i >= 0; i--) {
+            testNumbersList.add(i);
+        }
+
+        assertEquals(11, testNumbersList.size());
+
+        ArrayList.sort(testNumbersList);
+
+        for (int i = 0; i <= 10; i++) {
+            assertEquals(i, testNumbersList.get(i));
+        }
+    }
+
+    @Test
+    void testStringSort() {
+        testStringList.add("e");
+        testStringList.add("d");
+        testStringList.add("c");
+        testStringList.add("a");
+        testStringList.add("b");
+
+        ArrayList.sort(testStringList);
+
+        assertEquals("a", testStringList.get(0));
+        assertEquals("e", testStringList.get(4));
     }
 }
